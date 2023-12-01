95d223593ce7cc2c3b0425dce9e793d29bdb9699
SilentInt <silentint@foxmail.com>
2023-12-01 16:42:22 +0800

    ADD wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,15 @@
 local wezterm = require("wezterm")
--- local dracula = require("dracula")
-local config = {}
+local dracula = require("dracula")
 
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 16
-config.window_background_opacity = 0.6
-config.text_background_opacity = 0.8
-config.hide_tab_bar_if_only_one_tab = true
+-- 使用 Dracula 配色方案
+return {
+	font = wezterm.font_with_fallback({ "FiraCode Nerd Font", "JetBrains Mono", "WenQuanYi Micro Hei" }),
+	font_size = 14,
+	window_background_opacity = 0.8,
+	text_background_opacity = 0.8,
+	hide_tab_bar_if_only_one_tab = true,
+	colors = dracula,
 
--- config.color_scheme = "dracula"
--- config.tab_bar_at_bottom = true
--- config.use_fancy_tab_bar = false
-
-return config
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+}
